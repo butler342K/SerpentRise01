7edb443f9dcea7908ada245f8b0f394f476fe14f
Yurii <yuriikroitor@gmail.com>
2025-07-17 23:05:15 +0300

    Update errors

---
+++ src/errors.py
@@ -0,0 +1,17 @@
+class InvalidInputError(Exception):
+    pass
+
+class ContactNotFoundError(Exception):
+    pass
+
+class EmailNotSetError(Exception):
+    pass
+
+class AddressNotSetError(Exception):
+    pass
+
+class PhoneNotFoundError(Exception):
+    pass
+
+class AddressBookError(Exception):
+    pass
